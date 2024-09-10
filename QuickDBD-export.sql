f9451c9ffced36af48f440f8bd74007f0481b36f
Heidi Fox <heidi_fox831@yahoo.com>
2024-09-10 18:39:34 -0500

    Add files via upload

---
+++ QuickDBD-export.sql
@@ -0,0 +1,66 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "titles" (
+    "title_id" INTEGER   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "SALARIES" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    CONSTRAINT "pk_SALARIES" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "EMPLOYEES" (
+    "emp_no" INTEGER   NOT NULL,
+    "emp_title_id" INTEGER   NOT NULL,
+    "birth_date" INTEGER   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" INTEGER   NOT NULL
+);
+
+CREATE TABLE "DEPT_MANAGER" (
+    "dept_no" INTEGER   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    CONSTRAINT "pk_DEPT_MANAGER" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "DEPT_EMP" (
+    "emp_no" INTEGER   NOT NULL,
+    "Dept_no" INTEGER   NOT NULL
+);
+
+CREATE TABLE "departments" (
+    "dept_no" INTEGER   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL
+);
+
+ALTER TABLE "EMPLOYEES" ADD CONSTRAINT "fk_EMPLOYEES_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "SALARIES" ("emp_no");
+
+ALTER TABLE "EMPLOYEES" ADD CONSTRAINT "fk_EMPLOYEES_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "DEPT_MANAGER" ADD CONSTRAINT "fk_DEPT_MANAGER_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "SALARIES" ("emp_no");
+
+ALTER TABLE "DEPT_EMP" ADD CONSTRAINT "fk_DEPT_EMP_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "SALARIES" ("emp_no");
+
+ALTER TABLE "DEPT_EMP" ADD CONSTRAINT "fk_DEPT_EMP_Dept_no" FOREIGN KEY("Dept_no")
+REFERENCES "DEPT_MANAGER" ("dept_no");
+
+ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "DEPT_MANAGER" ("dept_no");
+
